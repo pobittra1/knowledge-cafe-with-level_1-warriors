58d9bbae4a6fa11228e4cfb1a9a234183aaf8f14
pobi <pobittrachandraroy2001@gmail.com>
2024-04-04 11:12:58 +0600

    overall i done that, projects setup with tailwindcss & done some operation like fetch data. and also create fake data from chatgpt. then added this data in local machine

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className='text-3xl font-semibold'>knowledge cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
